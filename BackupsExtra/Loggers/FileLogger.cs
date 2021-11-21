e98ecef8f91e8879f36f00d0e6cf4ff2f35adc9f
Andreev Artem <andreev_3731@mail.ru>
2021-11-21 16:12:05 +0300

    feat: add concrete loggers

---
+++ BackupsExtra/Loggers/FileLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using BackupsExtra.Tools;
+
+namespace BackupsExtra.Loggers
+{
+    public class FileLogger : ILogger
+    {
+        public FileLogger(string pathToFile, bool timeCodeRequired = false)
+        {
+            FilePath = pathToFile ?? throw new ArgumentNullException(nameof(pathToFile));
+            if (!Directory.Exists(new FileInfo(pathToFile).DirectoryName))
+                throw new LoggerException($"File at {pathToFile} unreachable!");
+
+            TimeCodeRequired = timeCodeRequired;
+        }
+
+        public string FilePath { get; }
+        public bool TimeCodeRequired { get; }
+
+        public void LogMessage(string message)
+        {
+            using StreamWriter sw = File.AppendText(FilePath);
+            sw.WriteLine($"{GetTimeCode()}{message}");
+        }
+
+        public void LogException(Exception exception)
+        {
+            using StreamWriter sw = File.AppendText(FilePath);
+            sw.WriteLine($"{GetTimeCode()}{exception.Message}");
+        }
+
+        private string GetTimeCode()
+        {
+            return TimeCodeRequired
+                ? $"[{DateTime.Now.ToString("g", CultureInfo.CurrentCulture)}] - "
+                : string.Empty;
+        }
+    }
+}
